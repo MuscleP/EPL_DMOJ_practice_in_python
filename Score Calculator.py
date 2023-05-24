2b39739f13fcfe44c39702010fa55aaa8b2649be
MuscleP <MusclePass@protonnmail.com>
2023-05-25 00:21:56 +0800

    to week 13

---
+++ Score Calculator.py
@@ -0,0 +1,13 @@
+from statistics import mean # you may use this function to find the average of a list of number
+
+def average(scores):
+    return map(mean, zip(*scores))
+# Note that the following code is for local testing purposes only. You should leave this part of code unchanged and not submit it to the OJ system.
+T = int(input())
+for t in range(T):
+    k = int(input())
+    scores = [map(int, input().split()) for _ in range(k)]
+    avers = average(scores)
+    for m in avers:
+        print(f'{m:.2f}', end=' ')
+    print()
